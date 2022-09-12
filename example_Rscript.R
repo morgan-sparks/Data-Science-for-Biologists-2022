0c4986837e580ecfc389ae9836d4c607a0b63778
morgan-sparks <msparks1309@gmail.com>
2022-09-12 09:16:06 -0400

    add example_Rscript.R

---
+++ example_Rscript.R
@@ -0,0 +1,37 @@
+# Header ------------------------------------------------------------------
+# Assignment name:
+# Author name:
+# Date:
+# Notes:
+
+# File setup --------------------------------------------------------------
+
+# set working directory below
+
+# load libraries below
+
+# Exercise X --------------------------------------------------------------
+
+### Question 1
+
+### Question 2
+
+# Exercise Y --------------------------------------------------------------
+
+### Question 1
+
+### Question 2
+
+
+# Helpful info ------------------------------------------------------------
+
+# Using Ctrl+Shift+R (Cmd+Shift+R on the Mac) creates new sections which are an easy way to organize
+# scripts. You can also use them to navigate around very large scripts whith the stacked line icon in
+# the top right of the script window.
+
+
+# Using Ctrl+Shift+C (Cmd+Shift+C on the Mac) creates multiple commented out lines (e.g., # ) and allows you
+# to type longer text segments and then comment them out as a group.
+
+
+
